d816beacffe5f384c1988917896518427e959d87
Wish <wishyt@proton.me>
2026-01-24 13:12:47 -0600

    add folder creation to setup

--- server/setup.py
+++ server/setup.py
@@ -1,4 +1,6 @@
 import scrypt
+import os.path
+import os
 print("welcome to swffy setup")
 print("please enter a password for the admin panel!! this is where you approve/reject flash stuff")
 pw = input()
@@ -6,3 +8,8 @@ encrypted = scrypt.encrypt(pw, pw)
 with open("adminpw.bin", "wb") as f:
     f.write(encrypted)
     print("password written and encrypted! have fun")
+ftm = ["db", "db/swf", "db/torrents"]
+for item in ftm:
+    if not os.path.exists(item):
+        os.mkdir(item)
+        print("folders required werent found and were made!")
